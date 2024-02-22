8b3ec110ea567785f884a13782c9e906d164e0d7
ADMISIONES <ADMISIONES@DESKTOP-3EUMTG9>
2024-02-22 14:29:21 -0600

    Agregar archivos de proyecto.

---
+++ Hello/Api.cs
@@ -0,0 +1,22 @@
+﻿namespace Hello
+{
+    public class Api
+    {
+        //public static void ConfigurarApi(this WebApplication app)
+        //{
+        //    app.MapGet(pattern: "/Users", GetUsers);
+        //}
+
+        public async Task<IResult> GetUsers(IUserData data)
+        {
+            try
+            {
+                return Results.Ok(await data.GetUsers());
+            }
+            catch (Exception ex) {
+                return Results.Problem(ex.Message);
+            }
+
+        }
+    }
+}
